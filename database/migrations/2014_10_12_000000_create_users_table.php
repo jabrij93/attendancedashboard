4f0890935c2864bd3cd3cbe1a12bf2f5e1fc9e67
jabss <jabrijuhinin93@gmail.com>
2023-08-11 23:27:36 +0800

    cleanup user db migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,21 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->binary('images')->nullable();
-            $table->string('staff_id')->unique()->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('gender')->nullable();
-            $table->string('department')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address')->nullable();
-            $table->integer('phonenumber')->nullable();
-            $table->string('company_name')->nullable();
-            $table->string('date_checkIn')->nullable();
-            $table->string('time_checkIn')->nullable();
-            $table->string('location_checkIn')->nullable();
-            $table->string('time_checkOut')->nullable();
-            $table->string('location_checkOut')->nullable();
             $table->string('role');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
