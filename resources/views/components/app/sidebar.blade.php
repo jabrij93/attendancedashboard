59b83713992be55566385474d8966a3213406d50
jabss <jabrijuhinin93@gmail.com>
2023-08-20 17:46:02 +0800

    update routing(better mthd - fix wrong url route)

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -102,7 +102,7 @@ $total = ItemController::cartItem();
                                     </a>
                                 </li>
                                 <li class="mb-1 last:mb-0">
-                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('sales.index')){{ '!text-indigo-500' }}@endif" href="sales">
+                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('sales.index')){{ '!text-indigo-500' }}@endif" href="{{ route('item.index') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Sell Items</span>
                                     </a>
                                 </li>
