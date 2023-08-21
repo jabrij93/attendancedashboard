c665127f469e8dfaa432f9e7cf4e538fd8584adc
jabss <jabrijuhinin93@gmail.com>
2023-08-22 02:04:43 +0800

    db seed for mycart working

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -92,7 +92,7 @@ $total = ItemController::cartItem();
                                     </a>
                                 </li>
                                 <li class="mb-1 last:mb-0">
-                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('sales.index')){{ '!text-indigo-500' }}@endif" href="sales">
+                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('sales.index')){{ '!text-indigo-500' }}@endif" href="{{ route('item.mycart') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">My Cart ({{ $total }}) </span>
                                     </a>
                                 </li>
